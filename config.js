403bea6b8326866737cfb7bb852255d3a4605b6e
hectorguo <hectorguo@live.com>
2016-11-27 18:21:38 -0800

    add whitelist for CORS

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 module.exports = {
     secret: 'lynking-secret',
     database: 'mongodb://hector:guo@ds041566.mlab.com:41566/hectorguo',
-    corsWhitelist: ['null','https://4113studio.com','http://4113studio.com','https://localhost','http://localhost']
+    corsWhitelist: ['null','https://4113studio.com','http://4113studio.com','https://localhost','http://localhost','https://lynking.github.io','http://lynking.us','https://lynking.us','http://lynking.in','https://lynking.in']
 }
